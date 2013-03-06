6b335b9b7eadcf94f96edbd54c67a5a111842bd8
edyother <ed@edyother.com>
2013-03-06 08:23:38 -0500

    rewrite by @sag47

--- abc2pdf.sh
+++ abc2pdf.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
+# convert abc to pdf
+abcm2ps "$1" -O - | ps2pdf -sPAPERSIZE=a4 - > "${1/%.abc/.pdf}"
 
-# Script for converting a collection tunes written in abc format to pdf and midi
+# write a midi file for each tune in the collection
+abc2midi "$1" -t -n 30
 
-a=$(basename $1 .abc)
-
-## Writes abc file to post script
-
-abcm2ps $a.abc -O $a.ps
-
-## Writes post script to pdf
-
-ps2pdf -sPAPERSIZE=a4 $a.ps
-
-## Remove post script
-
-rm $a.ps
-
-## Writes a midi file for each tune in collection. -t Makes each midi file name the tune name rather than a number. -n 30 Allows the file names to be up to 30 characters long.
-
-abc2midi $a.abc -t -n 30
-
-## Moves all the midi files into their own directory if one exists, if not, it makes the directory and moves them in.
-
-if [ -d midi ]; then
-	mv *.mid midi
-else mkdir midi
-	mv *.mid midi	
-fi
+# move all the midi files into their own directory
+mkdir -p midi
+mv *.mid midi/
