941c3fa7abedce28e5b6b1bcc8ab9c18abdad9e4
Ed Yother <ed@edyother.com>
2018-02-02 20:22:58 -0500

    Remove the function of making midi files

--- abc2pdf.sh
+++ abc2pdf.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # convert abc to pdf
 abcm2ps "$1" -O - | ps2pdf -sPAPERSIZE=a4 - > "${1/%.abc/.pdf}"
-
-# write a midi file for each tune in the collection
-abc2midi "$1" -t -n 30
-
-# move all the midi files into their own directory
-mkdir -p midi
-mv *.mid midi/
